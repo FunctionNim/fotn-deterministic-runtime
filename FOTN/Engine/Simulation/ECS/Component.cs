f99358c1ef54d72cb3e4135f5674bf6edca2875c
FunctionNim <dyrone2001@gmail.com>
2026-05-17 20:06:15 -0400

    Add ECS component interface

---
+++ FOTN/Engine/Simulation/ECS/Component.cs
@@ -0,0 +1,9 @@
+namespace FOTN.Engine.Simulation.ECS;
+
+/// <summary>
+/// Marker interface for ECS data containers.
+/// Components must remain deterministic, serializable, and free of gameplay logic.
+/// </summary>
+public interface IComponent
+{
+}
