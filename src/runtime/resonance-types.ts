8c266ca7708443688fdb9c413be25e5d49534377
FunctionNim <dyrone2001@gmail.com>
2026-05-19 06:55:50 -0400

    Add continuity runtime resonance types

---
+++ src/runtime/resonance-types.ts
@@ -0,0 +1,56 @@
+export enum FunctionType {
+  Brown = 'Brown',
+  Stone = 'Stone',
+  Red = 'Red',
+  Purple = 'Purple',
+  Gold = 'Gold',
+  Green = 'Green',
+  Blue = 'Blue',
+  Pink = 'Pink',
+  Black = 'Black',
+  White = 'White',
+  Bond = 'Bond',
+  Grey = 'Grey',
+}
+
+export enum RelationshipType {
+  Dependency = 'Dependency',
+  Feed = 'Feed',
+  Opposition = 'Opposition',
+  Synchronization = 'Synchronization',
+  Neutral = 'Neutral',
+}
+
+export enum ResonanceType {
+  Stable = 'Stable',
+  Pressured = 'Pressured',
+  Synchronized = 'Synchronized',
+  Fractured = 'Fractured',
+  Restoring = 'Restoring',
+}
+
+export enum EmotionalTrend {
+  Settling = 'Settling',
+  Rising = 'Rising',
+  Distorting = 'Distorting',
+  Synchronizing = 'Synchronizing',
+  Recovering = 'Recovering',
+}
+
+export enum EncounterPhase {
+  Dormant = 'Dormant',
+  Entering = 'Entering',
+  Escalating = 'Escalating',
+  Stabilizing = 'Stabilizing',
+  Resolved = 'Resolved',
+}
+
+export enum EventType {
+  PressureShift = 'PressureShift',
+  FunctionRelationshipChanged = 'FunctionRelationshipChanged',
+  EncounterEntered = 'EncounterEntered',
+  EncounterResolved = 'EncounterResolved',
+  RestorationApplied = 'RestorationApplied',
+  MemoryPropagated = 'MemoryPropagated',
+  SymbolMeaningShifted = 'SymbolMeaningShifted',
+}
