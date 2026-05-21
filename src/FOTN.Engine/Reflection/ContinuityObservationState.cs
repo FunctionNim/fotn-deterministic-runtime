e4913a038614a59fbbbcc06ea0d669ad6bf96037
FunctionNim <dyrone2001@gmail.com>
2026-05-21 10:28:46 -0400

    Add continuity observation state

---
+++ src/FOTN.Engine/Reflection/ContinuityObservationState.cs
@@ -0,0 +1,9 @@
+namespace FOTN.Engine.Reflection;
+
+public sealed record ContinuityObservationState
+(
+    int ActiveCivilizations,
+    int ActiveConvergences,
+    int ReplayArchives,
+    string ObservationHash
+);
