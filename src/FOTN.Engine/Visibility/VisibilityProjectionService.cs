61201c848b34715852df427a6421be367d670c4f
FunctionNim <dyrone2001@gmail.com>
2026-05-21 09:04:20 -0400

    Add visibility projection service

---
+++ src/FOTN.Engine/Visibility/VisibilityProjectionService.cs
@@ -0,0 +1,11 @@
+namespace FOTN.Engine.Visibility;
+
+public sealed class VisibilityProjectionService
+{
+    public InformationRevealState Project(
+        InformationRevealState state
+    )
+    {
+        return state;
+    }
+}
