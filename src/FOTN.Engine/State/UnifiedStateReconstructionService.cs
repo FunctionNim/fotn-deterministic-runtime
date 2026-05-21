9def6927b73977759c772a49e085b9a139fd7e88
FunctionNim <dyrone2001@gmail.com>
2026-05-21 09:21:55 -0400

    Add unified state reconstruction service

---
+++ src/FOTN.Engine/State/UnifiedStateReconstructionService.cs
@@ -0,0 +1,11 @@
+namespace FOTN.Engine.State;
+
+public sealed class UnifiedStateReconstructionService
+{
+    public UnifiedPlayState Reconstruct(
+        UnifiedPlayState state
+    )
+    {
+        return state;
+    }
+}
